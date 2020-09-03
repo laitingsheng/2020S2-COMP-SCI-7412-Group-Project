0c855941827a154a1c201cc7808c7d988d028b28
Tinson Lai <laitingsheng@hotmail.com>
2020-09-03 11:40:46 +0930

    Reinitialise Backend project with Groovy Gradle & Add Dependabot configuration

---
+++ Backend/src/test/java/dev/tinson/uoa/sse/groupproject/backend/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.tinson.uoa.sse.groupproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
