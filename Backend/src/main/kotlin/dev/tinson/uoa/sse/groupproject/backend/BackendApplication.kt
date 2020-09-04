88000edb6f4031ddf90cbea622888ebf7f943539
Tinson Lai <laitingsheng@hotmail.com>
2020-09-04 10:27:21 +0930

    Minor update

--- Backend/src/main/kotlin/dev/tinson/uoa/sse/groupproject/backend/BackendApplication.kt
+++ Backend/src/main/kotlin/dev/tinson/uoa/sse/groupproject/backend/BackendApplication.kt
@@ -1,10 +1,16 @@
+@file:JvmName("Main")
+
 package dev.tinson.uoa.sse.groupproject.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
 
 @SpringBootApplication
-class BackendApplication
+class BackendApplication {
+    @RequestMapping("/verify")
+    fun verify(): Boolean = false
+}
 
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
