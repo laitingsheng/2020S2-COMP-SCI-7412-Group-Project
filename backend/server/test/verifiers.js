fda9d02e97aa1c93f6f21e7b921eaf440705fe8e
Tinson Lai <laitingsheng@hotmail.com>
2020-09-29 01:25:05 +0930

    Add test case with GitHub Actions

--- backend/server/test/verifiers.js
+++ backend/server/test/verifiers.js
@@ -1,1 +1,17 @@
 import assert from "assert"
+
+import verifiers from "../src/verifiers.js"
+
+describe("verifier", function() {
+    for (const label in verifiers)
+        it(`${label} must have an appropriate signature and never fail`, function() {
+            const verifier = verifiers[label]
+            assert.ok(verifier instanceof Function)
+            assert.strictEqual(verifier.length, 1)
+            assert.ok(verifier({}) instanceof Promise)
+        })
+
+    it("DUMMY should always return true", async function() {
+        assert.strictEqual(await verifiers.DUMMY({}), true)
+    })
+})
