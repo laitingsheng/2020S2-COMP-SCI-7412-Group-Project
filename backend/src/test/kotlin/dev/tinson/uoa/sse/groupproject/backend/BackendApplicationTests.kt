37098cdfe79ba1badd8781e55c78aee8fdac26ae
Tinson Lai <laitingsheng@hotmail.com>
2020-09-30 02:34:19 +0930

    Remove Node.js & Add back Spring Boot Kotlin

---
+++ backend/src/test/kotlin/dev/tinson/uoa/sse/groupproject/backend/BackendApplicationTests.kt
@@ -0,0 +1,11 @@
+package dev.tinson.uoa.sse.groupproject.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
