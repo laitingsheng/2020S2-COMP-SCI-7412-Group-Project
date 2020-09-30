2d03b8338e1763b7514efbdcb36a4d361e1c3dd2
Tinson Lai <laitingsheng@hotmail.com>
2020-09-30 21:31:59 +0930

    Add publish to GitHub Actions

--- backend/src/main/kotlin/dev/tinson/uoa/sse/groupproject/backend/utils.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("Utils")
-
-package dev.tinson.uoa.sse.groupproject.backend
-
-import java.io.InputStream
-
-fun String.asResourceStream(): InputStream = javaClass.getResourceAsStream(this)
