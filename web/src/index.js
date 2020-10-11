39dc13815ba3577d61da7559ae7d5e8fdfc89eb5
Tinson Lai <laitingsheng@hotmail.com>
2020-10-12 01:24:38 +1030

    Remove unnecessary vendor but not tested yet

--- web/src/index.js
+++ web/src/index.js
@@ -8,6 +8,7 @@
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 
 * Coded by Creative Tim
+* Edited by Tinson Lai
 
 =========================================================
 
@@ -20,25 +21,20 @@ import ReactDOM from "react-dom";
 // react library for routing
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-// bootstrap rtl for rtl support page
-import "assets/vendor/bootstrap-rtl/bootstrap-rtl.scss";
-// plugins styles from node_modules
 import "react-notification-alert/dist/animate.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
-// plugins styles downloaded
-import "assets/vendor/fullcalendar/dist/fullcalendar.min.css";
-import "assets/vendor/sweetalert2/dist/sweetalert2.min.css";
-import "assets/vendor/select2/dist/css/select2.min.css";
-import "assets/vendor/quill/dist/quill.core.css";
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
-// core styles
-import "assets/scss/argon-dashboard-pro-react.scss?v1.1.0";
+import "fullcalendar/main.css";
+import "quill/dist/quill.core.css";
+import "select2/dist/css/select2.min.css";
+import "sweetalert2/dist/sweetalert2.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-import AdminLayout from "layouts/Admin.js";
-import RTLLayout from "layouts/RTL.js";
-import AuthLayout from "layouts/Auth.js";
-import IndexView from "views/Index.js";
+import "assets/vendor/nucleo/css/nucleo.css";
+
+import "assets/scss/argon-dashboard-pro-react.scss";
+
+import AdminLayout from "./layouts/Admin.js";
+import AuthLayout from "./layouts/Auth.js";
 
 // // Firebase App (the core Firebase SDK) is always required and must be listed first
 // import * as firebase from "firebase/app";
@@ -72,14 +68,12 @@ import IndexView from "views/Index.js";
   
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Route path="/rtl" render={props => <RTLLayout {...props} />} />
-      <Route path="/auth" render={props => <AuthLayout {...props} />} />
-      <Route path="/" render={props => <IndexView {...props} />} />
-      <Redirect from="*" to="/" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/admin" render={props => <AdminLayout {...props} />} />
+            <Route path="/" render={props => <AuthLayout {...props} />} />
+            <Redirect from="*" to="/" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
