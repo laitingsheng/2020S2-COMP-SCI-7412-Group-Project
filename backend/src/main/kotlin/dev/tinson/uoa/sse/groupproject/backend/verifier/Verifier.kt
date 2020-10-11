0ffa8165698466b2f6aae0e620fe658206471126
Tinson Lai <laitingsheng@hotmail.com>
2020-10-11 22:53:55 +1030

    Remove unnecessary backend

--- backend/src/main/kotlin/dev/tinson/uoa/sse/groupproject/backend/verifier/Verifier.kt
+++
@@ -1,8 +0,0 @@
-package dev.tinson.uoa.sse.groupproject.backend.verifier
-
-import com.google.cloud.firestore.DocumentSnapshot
-
-interface Verifier {
-    val worth: Int
-    operator fun invoke(fields: DocumentSnapshot): Boolean
-}
