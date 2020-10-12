2f749565017c4e516198c8399bd802cffd4bca2d
Tinson Lai <laitingsheng@hotmail.com>
2020-10-12 16:14:38 +1030

    Add Firebase config

--- web/src/index.js
+++ web/src/index.js
@@ -18,7 +18,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-// react library for routing
+
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "react-notification-alert/dist/animate.css";
@@ -36,36 +36,18 @@ import "assets/scss/argon-dashboard-pro-react.scss";
 import AdminLayout from "./layouts/Admin.js";
 import AuthLayout from "./layouts/Auth.js";
 
-// // Firebase App (the core Firebase SDK) is always required and must be listed first
-// import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 
-// // If you enabled Analytics in your project, add the Firebase SDK for Analytics
-// import "firebase/analytics";
-
-// // Add the Firebase products that you want to use
-// import "firebase/auth";
-// import "firebase/firestore";
-// import "firebase/messaging";
-
-// // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBRn5Nf1_4JZggwEgF5ttKTePsAJg8qKe8",
-//     authDomain: "comp-sci-7412-group-project.firebaseapp.com",
-//     databaseURL: "https://comp-sci-7412-group-project.firebaseio.com",
-//     projectId: "comp-sci-7412-group-project",
-//     storageBucket: "comp-sci-7412-group-project.appspot.com",
-//     messagingSenderId: "915849829476",
-//     appId: "1:915849829476:web:6ef7b7b2b620d1cabf5371",
-//     measurementId: "G-D9QL9W718S"
-//   };
-//   // Initialize Firebase
-//   var defaultProject = firebase.initializeApp(firebaseConfig);
-//   console.log(defaultProject.name);  // "[DEFAULT]"
-//   const auth = firebase.auth();
-//   const db = firebase.firestore();
-//   // //update firebase libary
-//   // firebase.firestore().settings({timestampsInSnapshots: true});
-  
+firebase.initializeApp({
+    apiKey: "AIzaSyBRn5Nf1_4JZggwEgF5ttKTePsAJg8qKe8",
+    authDomain: "comp-sci-7412-group-project.firebaseapp.com",
+    databaseURL: "https://comp-sci-7412-group-project.firebaseio.com",
+    projectId: "comp-sci-7412-group-project",
+    storageBucket: "comp-sci-7412-group-project.appspot.com",
+    messagingSenderId: "915849829476",
+    appId: "1:915849829476:web:6ef7b7b2b620d1cabf5371",
+    measurementId: "G-D9QL9W718S"
+});
 
 ReactDOM.render(
     <BrowserRouter>
